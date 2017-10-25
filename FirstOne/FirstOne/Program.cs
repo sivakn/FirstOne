28bc00b347d64699a344ce4eafbf07c3ee8bb702
Siva Prakasam <piratesiva@gmail.com>
2017-10-25 16:45:23 +0530

    commit one

---
+++ FirstOne/FirstOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Count One!!!");
+        }
+    }
+}
